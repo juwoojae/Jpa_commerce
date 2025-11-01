74d529ad7e34e7fc45b7f1509ad14d246a171fab
Juwoojae <juwqq1234@naver.com>
2025-11-01 23:07:32 +0900

    Initial commit

---
+++ src/main/java/com/example/jpa_commerce/JpaCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCommerceApplication.class, args);
+    }
+
+}
